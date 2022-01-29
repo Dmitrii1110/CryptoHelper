fab26caa3f53aeea871e91297ac63675e10f42f2
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2022-01-29 14:20:01 +0300

    Подготовка CryptoTableViewCell

---
+++ CryptoHelper/CryptoTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyTableViewCell.swift
+//  CryptoHelper
+//
+//  Created by admin1 on 29.01.2022.
+//
+
+import UIKit
+
+class CryptoTableViewCell: UITableViewCell {
+
+    static let identifier = "CryptoTableViewCell"
+
+}
--- CryptoHelper/ViewController.swift
+++ CryptoHelper/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("View Loaded")
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CryptoTableViewCell.identifier, for: indexPath) as? CryptoTableViewCell
+        else {
+            fatalError()
+        }
+        return cell
+    }
 
 }
 
